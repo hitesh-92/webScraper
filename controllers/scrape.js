82e74331042fa8fa0f64d3ca0d9a520e5f2a332a
hitesh <hiteshgohil@hotmail.co.uk>
2019-06-03 22:07:48 +0100

    yahoo rss feed added

---
+++ controllers/scrape.js
@@ -0,0 +1,29 @@
+async function yahoo(req, res, request, cheerio){
+
+  const url = 'https://www.yahoo.com/news/rss';
+  return await request(url, handleHTML);
+
+  function handleHTML(err, response, html){
+    if(err && response.statusCode != 200) {
+      console.log('Error')
+      return res.json({error: 'yahoo'});
+    }
+
+    const data = [];
+	
+    const $ = cheerio.load(html, {
+	    normalizeWhitespace: true,
+	    xmlMode: true
+    });
+	
+	$('item').each( (i, el) => {
+		const title = $(el).children()['0'].children[0].data
+		const link = $(el).children()['2'].children[0].data
+		data.push({title, link});
+	});
+	
+    res.json(data);
+  };
+};
+
+module.exports = {yahoo}
--- server.js
+++ server.js
@@ -3,10 +3,14 @@ const fs = 	require('fs');
 const request = require('request');
 const cheerio = require('cheerio');
 
+const Scrape =	require('./controllers/scrape'); 
+
 const app = express();
 const PORT  = process.env.PORT || 5050;
 
-app.get('', (req, res) => res.json('running'));
+app.get('', (req, res) => res.json('gReport Scraper'));
+
+app.get('/scrape/yahoo', (req, res) => { Scrape.yahoo(req, res, request, cheerio) });
 
 app.listen(PORT, () => {
   console.log(`\x1Bc
